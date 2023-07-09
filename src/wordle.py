9a3252c362e8f5d1a5b766772930dddef1203b6e
Mobina Jamali <mobina.jamali.mj@gmail.com>
2023-07-09 00:12:02 -0600

    update

--- src/wordle.py
+++ src/wordle.py
@@ -1,9 +1,15 @@
+import random
+from src.utils import print_error, print_success, print_warning
+
+random.seed(42)
+
 class Wordle:
     def __init__(self, file_path: str, word_len: int = 5, limit: int = 10000):
+        self.word_len = word_len
         self.words = self.generate_word_frequency(file_path, word_len, limit)
 
     #Building Data
-    def generate_word_frequency(file_path, word_len: int, limit: int):
+    def generate_word_frequency(self, file_path, word_len: int, limit: int):
         #building data: a list of tuples of words with their corresponding counts
         words_freq = []
         with open(file_path) as f:
@@ -26,3 +32,54 @@ class Wordle:
         words = list(filter(lambda w: len(w) == word_len, words))
 
         return words
+
+    def run(self, ):
+        # Random Word
+        word = random.choice(self.words)
+        word = word.upper()
+
+        # Start Game!
+        num_try = 6
+        success = False
+
+        while num_try:
+            guess_word = input(f'Enter a {self.word_len} letter word (or q to exit):')
+            if guess_word.lower() == 'q':
+                break
+            guess_word = guess_word.upper()
+
+            #word length
+            if len(guess_word) != 5:
+                print(f'Word must have {self.word_len} letters. You entered {len(guess_word)}!')
+                continue
+
+            # Check valid word
+            if guess_word.lower() not in self.words:
+                print_warning('Word is not valid!')
+                continue
+
+            # Check valid, invalid positions, invalid characters
+            for w_letter, g_letter in zip(word, guess_word):
+                if w_letter == g_letter:
+                    print_success(f' {g_letter} ', end='')
+                    print(' ', end='')
+                elif g_letter in word:
+                    print_warning(f' {g_letter} ', end='')
+                    print(' ', end='')
+                else:
+                    print_error(f' {g_letter} ', end='')
+                    print(' ', end='')
+            print()
+
+            # Check success
+            if word == guess_word:
+                print()
+                print_success(' Congradulations! ')
+                success = True
+                break
+
+            num_try -=1
+
+        if not success:
+            print_warning(f'Game over: The word was "{word}"!')
+
